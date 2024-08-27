d95d7f7b143825076cf8556671a898ce5fe51827
jbrak <joe@brakoniecki.com>
2024-08-27 09:20:36 +0100

    Added Recap

---
+++ Recap/1.py
@@ -0,0 +1,9 @@
+# Define a variable called num1 that saves the result of an integer input from the user
+
+
+# Cube num1 and then add 2 to it, save this new number to a variable called num2.
+
+
+# Print the sentence: ________ cubed +2 is ******. Replace ________ with the value of num1 and replace ****** with
+# the value of num2
+# Eg if my number was 2 the following sentence would be outputted: 2 cubed and +2 is 10
